86fb2f0e13a9d4c888b28728440921853fa94862
DaltonBreedt <151674144+DaltonBreedt@users.noreply.github.com>
2024-10-15 13:04:35 +0200

    12

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-885.js
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-885.js
@@ -1,7 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-06344e7c' );
-});
-jQuery(document).ready(function () {
+document.addEventListener("DOMContentLoaded", function(){ jQuery(document).ready(function () {
 				let scope = jQuery(".wp-block-uagb-image-gallery.uagb-block-2f34c3b2");
 				if ( scope.length ) { 
 					scope.css("visibility", "visible");
@@ -10,13 +7,4 @@ jQuery(document).ready(function () {
 						getSlickCarousel.slick({"arrows":false,"dots":false,"initialSlide":0,"infinite":false,"autoplay":true,"autoplaySpeed":2000,"pauseOnHover":true,"speed":500,"slidesToShow":4,"prevArrow":"<button type='button' data-role='none' class='spectra-image-gallery__control-arrows spectra-image-gallery__control-arrows--carousel slick-prev slick-arrow' aria-label='Previous' tabindex='0' role='button'><svg xmlns='http:\/\/www.w3.org\/2000\/svg' viewBox='0 0 256 512' width='24' height='24'><path d='M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z'><\/path><\/svg><\/button>","nextArrow":"<button type='button' data-role='none' class='spectra-image-gallery__control-arrows spectra-image-gallery__control-arrows--carousel slick-next slick-arrow' aria-label='Previous' tabindex='0' role='button'><svg xmlns='http:\/\/www.w3.org\/2000\/svg' viewBox='0 0 256 512' width='24' height='24'><path d='M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z'><\/path><\/svg><\/button>","rtl":false,"responsive":[{"breakpoint":1024,"settings":{"slidesToShow":4}},{"breakpoint":767,"settings":{"slidesToShow":2}}]});
 					}
 				}
-			});window.addEventListener( 'load', function() {
-	UAGBCounter.init( '.uagb-block-5263f268', {"layout":"number","heading":"Air Conditioning Repairs and Install","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":150,"totalNumber":150,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
-});
-window.addEventListener( 'load', function() {
-	UAGBCounter.init( '.uagb-block-bac7fc99', {"layout":"number","heading":"Air Conditioning Repairs and Services","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":100,"totalNumber":150,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
-});
-window.addEventListener( 'load', function() {
-	UAGBCounter.init( '.uagb-block-31a7865c', {"layout":"number","heading":"Customizers are Satisfied with our Services","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":250,"totalNumber":250,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
-});
- });
+			}); });
